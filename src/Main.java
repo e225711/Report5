68f69d7004d2524f3d3ea1eee8ba75afd4b414af
Maimu Kohagura <e225711@ie.u-ryukyu.ac.jp>
2022-12-11 02:10:43 +0900

    done for step3

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    try {
-      //処理したいコード
-      String str = null;
-      System.out.println(str.length());
-  }  catch (NullPointerException e) {
-      //NullPointerExceptionが起きたときの処理
-      System.out.println("NullPointerExceptionが発生");
-      System.out.println(e.getMessage());
-  }
+    String str = "壱百満";
+    int value = Integer.parseInt(str);
+    System.out.println(value);
   }
 }
